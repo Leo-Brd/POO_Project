967f005cf6ac6c1c0e5e506bdd889e0cfdee27d4
Leo-Brd <leobordet.pro@gmail.com>
2024-03-20 10:37:44 +0100

    Ex3_2

---
+++ exercice3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class Weapon{
+    public:
+
+        char name[20];
+        int damage;
+
+};
+
+class RPCharacter{
+    private:
+
+        char name[15];
+        int level;
+        int xp_points;
+        int hp;
+        Weapon weapon_list[10];
+        int weapon_quantity;
+        Weapon weapon_used;
+
+    public:
+
+        const char* getName() const { return name; }
+        void setName(const char* newName) {
+            strncpy(name, newName, 14);
+            name[14] = '\0';
+    }
+
+
+}
+
+
+
+int main(void){
+
+
+
+
+
+};
