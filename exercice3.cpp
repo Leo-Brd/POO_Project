7d70d56216a6f6cc1697231ed9cd7b04c761313d
Leo-Brd <leobordet.pro@gmail.com>
2024-03-21 11:42:27 +0100

    Ex3_7

--- exercice3.cpp
+++ exercice3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 class Weapon{
     private:
@@ -10,6 +11,7 @@ class Weapon{
     public:
 
         Weapon() : name("Hand"), damage(1) {}
+
         Weapon(const char newName[], int newDamage) : damage(newDamage) {
             strncpy(name, newName, sizeof(name) - 1); 
             name[sizeof(name) - 1] = '\0'; 
@@ -27,11 +29,26 @@ class RPCharacter{
         int level;
         int xp_points;
         int hp;
-        Weapon weapon_list[10];
+        std::vector<Weapon> weapon_list;
         int weapon_quantity;
         Weapon weapon_used;
+        bool is_dead;
 
     public:
+
+        RPCharacter(const char newName[]) {
+            strncpy(name, newName, sizeof(name) - 1); 
+            name[sizeof(name) - 1] = '\0'; 
+
+            level = 1;
+            xp_points = 0;
+            hp = 100;
+            weapon_quantity = 0;
+            weapon_list.push_back(Weapon());
+            weapon_used = Weapon();
+            is_dead = false;
+
+        }
 
         const char* getName() const { return name; }
         void setName(const char* newName) {
